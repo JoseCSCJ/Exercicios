3138751d3c0c9eb014d0c23a77faa8a3d837eb69
opet <josecarlos.jr31@gmail.com>
2018-09-05 20:54:37 -0300

    Exer2 parcial1

--- exercicio2/src/main/java/com/example/exercicio2/Exercicio2.java
+++ exercicio2/src/main/java/com/example/exercicio2/Exercicio2.java
@@ -2,12 +2,25 @@ package com.example.exercicio2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class exercicio2 extends Activity {
+
+    private EditText editIdade;
+    private TextView textCampeonato;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercicio2);
+
+
+    }
+
+    public void categorizar (View view){
+
     }
 }
