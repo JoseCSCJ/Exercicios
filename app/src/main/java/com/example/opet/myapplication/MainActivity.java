ce94339d6e8df43624f08fb67610749ec5f91cd0
opet <josecarlos.jr31@gmail.com>
2018-09-05 20:11:34 -0300

    Parcial3

--- app/src/main/java/com/example/opet/myapplication/MainActivity.java
+++ app/src/main/java/com/example/opet/myapplication/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,16 +25,31 @@ public class MainActivity extends AppCompatActivity {
         editPedido = findViewById(R.id.editPedido);
         editLimite = findViewById(R.id.editLimite);
         textLimiteAtual = findViewById(R.id.textLimiteAtual);
+        textPedidoAtual = findViewById(R.id.textPedidoAtual);
 
     }
 
     public void calculaSoma(View view){
 
-        double total = Double.parseDouble()
-        double pedido =
+        double total = Double.parseDouble(textPedidoAtual.getText().toString());
+        double pedido = Double.parseDouble(editPedido.getText().toString());
+        double ultrap = 0;
+
+        total = total + pedido;
+
+        DecimalFormat mFormat = new DecimalFormat(".00");
+
+        textPedidoAtual.setText(mFormat.format(total));
+
+        if (total>Double.parseDouble(textLimiteAtual.getText().toString()))
+            ultrap = total - Double.parseDouble(textLimiteAtual.getText().toString());
+            String textToast = "Você ultrapassou o limite por " + ultrap;
+            Toast.makeText(getApplicationContext(), textToast, Toast.LENGTH_SHORT);
+
 
     }
     public void atualizarLimite (View view){
+
 
     }
 }
